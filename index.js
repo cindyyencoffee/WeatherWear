67070a16ada5605072af47f4321dceef7cc7b7e7
Cindy Yen <cindyyen92529@yahoo.com.tw>
2015-01-16 14:01:54 +0800

    一月十六js

--- index.js
+++ index.js
@@ -1,37 +1,85 @@
+<meta name="robots" content="noindex">
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>天氣選取</title>
+<style id="jsbin-css">
+html{
+  background-color:white;
 
-angular.module('wearApp',[])
-
-.controller('myCtrl', ['$scope','$window','$http', myCtrl]);
-
-function myCtrl($scope,$window,$http) {
-  
-  //models
-  angular.extend($scope, {
-      weather: {
-        result: [{items: [{date:'讀取中'}]}]
-      }
-    }  
-  );  
-
- 
-  //methods
-  
-  angular.extend($scope, {    
-    ezGet : function(url, t){
-      $http.get(url)
-        .success(function(data,   status, headers, config) {
-        console.log(data);
-        $scope[t] = data;
-      })
-      
-      .error(function(data, status, headers, config) {
-        console.log(status);
-      });
-  
-    }      
-  });
-  
- $scope.ezGet('http://www.corsproxy.com/twweatherapi.herokuapp.com/week2?location=all&output=json','weather');
-  
-    
 }
+.rain{
+ margin-left:470px;
+  
+}
+.sun{
+   margin-left:820px;
+   margin-top:-20px;
+  
+}
+.cloudy{
+  margin-top:300px;
+  margin-left:470px;
+  
+}
+.snowman{
+ margin-top:300px;
+ margin-left:820px;
+ width:400px;
+  
+}
+#w{
+  width:300px;
+  position:absolute;
+
+}
+.circle{
+  width:300px;
+  height:300px;
+  border-radius:150px;
+  background-color:black;
+  margin-left:100px;
+  margin-top:200px;
+  position:absolute;
+  
+}
+#today{
+  color:white;
+  font-family:Songti TC;
+  margin-left:165px;
+  margin-top:340px;
+  position:absolute;
+  font-size:20px;
+}
+#w:hover,#w:active{
+  opacity:0;
+  transition:0s;
+}
+.word{
+  font-family:arial black ;
+  font-size:20px;
+  position:absolute;
+  opacity:0;
+}
+#rainy{
+  margin-left:570px;
+  margin-top:100px;
+}  
+#sunny{
+  margin-left:920px;
+  margin-top:100px;
+}
+#cloudy{
+  margin-left:570px;
+  margin-top:450px;
+}
+#cold{
+  margin-left:920px;
+  margin-top:450px;
+}
+#rain:hover >#rainy,#rain:active >#rainy{
+  opacity:0;
+  transition:0s;
+}
+</body>
+</html>
