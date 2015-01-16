e4b3c5d926a17b74aa112831cb582d2860b07bd5
Cindy Yen <cindyyen92529@yahoo.com.tw>
2015-01-16 14:14:48 +0800

    js1

--- index.js
+++ index.js
@@ -1,139 +1,79 @@
-<meta name="robots" content="noindex">
-<html>
-<head>
-  <meta charset="utf-8">
-  <title>選擇分類</title>
-<style id="jsbin-css">
 html{
   background-color:white;
+
+}
+.rain{
+ margin-left:470px;
   
 }
-body{
-  margin-left:400px;
-  max-width: 620px;
-}
-span{
-  margin-left:400px;
-}
-.note4{
-  font-size: 26px;
-  position:absolute;
-  top:450px;
-  left:330px;
-  z-index:1;
-}
-.note3{
-  font-size: 26px;
-  position:absolute;
-  top:450px;
-  left:80px;
-  z-index:1;
-}
-.note2{
-  font-size: 26px;
-  position:absolute;
-  top:100px;
-  left:330px;
-  z-index:1;
-}
-.note {
-  font-size: 26px;
-  position:absolute;
-  top:100px;
-  left:80px;
-  z-index:1;
+.sun{
+   margin-left:820px;
+   margin-top:-20px;
   
 }
-.front {
+.cloudy{
+  margin-top:300px;
+  margin-left:470px;
   
-  position:relative;
-  z-index:1;
+}
+.snowman{
+ margin-top:300px;
+ margin-left:820px;
+ width:400px;
+  
+}
+#w{
+  width:300px;
+  position:absolute;
+
 }
 .circle{
-  width:150px;
-  height:150px;
+  width:300px;
+  height:300px;
   border-radius:150px;
-  margin-top:-400px;
-  margin-left:150px;
-  background-color:white;
-}
-.style{
-  width:200px;
-  height:300px;
-  opacity:0.6;
- 
-}
-.one{
   background-color:black;
-  width:200px;
-  height:300px;
-  border:3px solid;
+  margin-left:100px;
+  margin-top:200px;
+  position:absolute;
   
 }
-.two{
-  background-color:black;
-  width:200px;
-  height:300px;
-  border:3px solid;
-  margin-left:250px;
-  margin-top:-305px;
-
-}
-.three{
-  background-color:black;
-  width:200px;
-  height:300px;
-  border:3px solid;
-  margin-top:40px;
-}
-.four{
-  background-color:black;
-  width:200px;
-  height:300px;
-  border:3px solid;
-  margin-left:250px;
-  margin-top:-305px;
-}
-.choose{
-  color:black;
-  font-size: 10px;
+#today{
+  color:white;
+  font-family:Songti TC;
+  margin-left:165px;
+  margin-top:340px;
   position:absolute;
-  top:335px;
-  left:183px;
-  z-index:1;
+  font-size:20px;
 }
-.style{
+#w:hover,#w:active{
+  opacity:0;
+  transition:0s;
+}
+.word{
+  font-family:arial black ;
+  font-size:20px;
+  position:absolute;
   opacity:0;
 }
-#nonote{
- color:white;
- font-family:Songti TC;
+#rainy{
+  margin-left:570px;
+  margin-top:100px;
+}  
+#sunny{
+  margin-left:920px;
+  margin-top:100px;
 }
-#words{
- font-family:Songti TC;
- margin-top:-7px;
-
+#cloudy{
+  margin-left:570px;
+  margin-top:450px;
 }
-#nonote:hover,#nonote:active{
-  opacity:0;  
+#cold{
+  margin-left:920px;
+  margin-top:450px;
+}
+#rain:hover >#rainy,#rain:active >#rainy{
+  opacity:0;
   transition:0s;
- 
 }
-#break:hover ,#break:active {
-  opacity:1;
- 
-}
-#official:hover,#official:active{
-  opacity:1;
-  
-}
-#simple:hover,#simple:active{
-  opacity:1;
-}
-#mixmatch:hover,#mixmatch:active{
-  opacity:1;
-}
-
-
 </style>
 </head>
