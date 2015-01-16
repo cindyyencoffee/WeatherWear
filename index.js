057086217b746498c062d78ead97f4ccf6c1b8ea
Cindy Yen <cindyyen92529@yahoo.com.tw>
2015-01-16 14:13:04 +0800

    js

--- index.js
+++ index.js
@@ -1,37 +1,139 @@
-
-angular.module('wearApp',[])
-
-.controller('myCtrl', ['$scope','$window','$http', myCtrl]);
-
-function myCtrl($scope,$window,$http) {
+<meta name="robots" content="noindex">
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>選擇分類</title>
+<style id="jsbin-css">
+html{
+  background-color:white;
   
-  //models
-  angular.extend($scope, {
-      weather: {
-        result: [{items: [{date:'讀取中'}]}]
-      }
-    }  
-  );  
-
- 
-  //methods
-  
-  angular.extend($scope, {    
-    ezGet : function(url, t){
-      $http.get(url)
-        .success(function(data,   status, headers, config) {
-        console.log(data);
-        $scope[t] = data;
-      })
-      
-      .error(function(data, status, headers, config) {
-        console.log(status);
-      });
-  
-    }      
-  });
-  
- $scope.ezGet('http://www.corsproxy.com/twweatherapi.herokuapp.com/week2?location=all&output=json','weather');
-  
-    
 }
+body{
+  margin-left:400px;
+  max-width: 620px;
+}
+span{
+  margin-left:400px;
+}
+.note4{
+  font-size: 26px;
+  position:absolute;
+  top:450px;
+  left:330px;
+  z-index:1;
+}
+.note3{
+  font-size: 26px;
+  position:absolute;
+  top:450px;
+  left:80px;
+  z-index:1;
+}
+.note2{
+  font-size: 26px;
+  position:absolute;
+  top:100px;
+  left:330px;
+  z-index:1;
+}
+.note {
+  font-size: 26px;
+  position:absolute;
+  top:100px;
+  left:80px;
+  z-index:1;
+  
+}
+.front {
+  
+  position:relative;
+  z-index:1;
+}
+.circle{
+  width:150px;
+  height:150px;
+  border-radius:150px;
+  margin-top:-400px;
+  margin-left:150px;
+  background-color:white;
+}
+.style{
+  width:200px;
+  height:300px;
+  opacity:0.6;
+ 
+}
+.one{
+  background-color:black;
+  width:200px;
+  height:300px;
+  border:3px solid;
+  
+}
+.two{
+  background-color:black;
+  width:200px;
+  height:300px;
+  border:3px solid;
+  margin-left:250px;
+  margin-top:-305px;
+
+}
+.three{
+  background-color:black;
+  width:200px;
+  height:300px;
+  border:3px solid;
+  margin-top:40px;
+}
+.four{
+  background-color:black;
+  width:200px;
+  height:300px;
+  border:3px solid;
+  margin-left:250px;
+  margin-top:-305px;
+}
+.choose{
+  color:black;
+  font-size: 10px;
+  position:absolute;
+  top:335px;
+  left:183px;
+  z-index:1;
+}
+.style{
+  opacity:0;
+}
+#nonote{
+ color:white;
+ font-family:Songti TC;
+}
+#words{
+ font-family:Songti TC;
+ margin-top:-7px;
+
+}
+#nonote:hover,#nonote:active{
+  opacity:0;  
+  transition:0s;
+ 
+}
+#break:hover ,#break:active {
+  opacity:1;
+ 
+}
+#official:hover,#official:active{
+  opacity:1;
+  
+}
+#simple:hover,#simple:active{
+  opacity:1;
+}
+#mixmatch:hover,#mixmatch:active{
+  opacity:1;
+}
+
+
+</style>
+</head>
